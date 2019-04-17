bf1c06a0b566387d9a22c5cab1b7d55574a69e48
gvernard <gvernard@astro.rug.nl>
2019-04-17 10:27:59 +0200

    Newer version of the python code that works with the latest LSST packages (see instructions in README) and Opsims.

--- lsst_part.py
+++ lsst_part.py
@@ -2,20 +2,20 @@ import json
 import sys
 import numpy
 import random
+
 import lsst.sims.maf.db as db
-import lsst.sims.maf.utils as utils
 import lsst.sims.maf.metrics as metrics
 import lsst.sims.maf.slicers as slicers
 import lsst.sims.maf.metricBundles as metricBundles
+
 from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 
 
-'''
-This code gets the ra and dec as an input, together with some LSST parameters (years, opsims file).
-The output is 6 files, one for each LSST filter, having the Julian date and depth in each column.
-These files can be empty if no LSST observation is planned on the given time scale.
-'''
+# This code gets the ra and dec as an input, together with some LSST parameters (years, opsims file).
+# The output is 6 files, one for each LSST filter, having the Julian date and depth in each column.
+# These files can be empty if no LSST observation is planned on the given time scale.
+
 
 
 if len(sys.argv) > 1:
@@ -25,7 +25,7 @@ else:
 
 
 
-print ">>>>>>>>>>>>>>>>>> Reading input <<<<<<<<<<<<<<<<<<<<<<<<<<<"
+print(">>>>>>>>>>>>>>>>>> Reading input <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 with open(inp_file) as json_input:    
     myinput = json.load(json_input)
     
@@ -38,7 +38,7 @@ days = myinput['lsst']['years']*365.25 # in days
 
 
 
-print ">>>>>>>>>>>> Calculating angular diameter distances <<<<<<<<<<<<<"
+print(">>>>>>>>>>>>>>>>>> Calculating angular diameter distances <<<<<<<<<<<<")
 H0 = 67.7 # in km s^-1 Mpc^-1
 Om0 = 0.309 # Omega matter at t=t_0 (now)
 cosmo = FlatLambdaCDM(H0=H0 * u.km / u.s / u.Mpc, Om0=Om0)
@@ -48,18 +48,18 @@ Dls = cosmo.angular_diameter_distance_z1z2(myinput['other']['zl'],myinput['other
 
 
 
-print ">>>>>>>>>>>>>>>>>> Reading LSST dates and depths <<<<<<<<<<<<<<<<<<<<<<<"
+print(">>>>>>>>>>>>>>>>>> Reading LSST dates and depths <<<<<<<<<<<<<<<<<<<<<")
 outDir = outdir+'/dates'
 dbFile = path_to_dbfile + runName + '_sqlite.db'
-opsimdb = utils.connectOpsimDb(dbFile)
+opsimdb = db.opsimDatabase.OpsimDatabase(dbFile)
 resultsDb = db.ResultsDb(outDir=outDir)
 
-ra=ra*15.0
+#ra=ra*15.0
 # SNR limit (Don't use points below this limit)
 snrLimit = 5.
 # The pass metric just passes data straight through.
-metric = metrics.PassMetric(cols=['filter','fiveSigmaDepth','expMJD'])
-slicer = slicers.UserPointsSlicer(ra,dec,lonCol='ditheredRA',latCol='ditheredDec')
+metric = metrics.PassMetric(cols=['filter','fiveSigmaDepth','observationStartMJD'])
+slicer = slicers.UserPointsSlicer(ra,dec,lonCol='fieldRA',latCol='fieldDec')
 sql = ''
 bundle = metricBundles.MetricBundle(metric,slicer,sql)
 bg =  metricBundles.MetricBundleGroup({0:bundle},opsimdb,outDir=outDir,resultsDb=resultsDb)
@@ -67,18 +67,18 @@ bg.runAll()
 bundle.metricValues.data[0]['filter']
 
 filters = ['u','g','r','i','z','y']
-print '%i Observations total at this point (All SNR levels)' % bundle.metricValues.data[0].size
+print('%i Observations total at this point (All SNR levels)' % bundle.metricValues.data[0].size)
 for fname in filters:
     good = numpy.where(bundle.metricValues.data[0]['filter'] == fname)
-    print '%i Observations in %s' % (good[0].size, fname)
-    mags=bundle.metricValues.data[0]['expMJD'][good[0]],bundle.metricValues.data[0]['fiveSigmaDepth'][good[0]]
+    print('%i Observations in %s' % (good[0].size, fname))
+    mags=bundle.metricValues.data[0]['observationStartMJD'][good[0]],bundle.metricValues.data[0]['fiveSigmaDepth'][good[0]]
     mags=numpy.array(mags)
     mags=numpy.transpose(mags)
     mags=mags[mags[:,0].argsort()]
-    numpy.savetxt(outDir+'/'+fname+".dat",header="JD   5-sigma-depth",numpy.c_[mags[:,0],mags[:1]])
+    numpy.savetxt(outDir+'/'+fname+".dat",numpy.c_[mags[:,0],mags[:,1]],header="JD   5-sigma-depth")
 
 
-print ">>>>>>>>>>>>>>>>>> Write json input for the GERLUMPH part <<<<<<<<<<<<<<<<<<<<<<<"
+print(">>>>>>>>>>>>>>>>>> Write json input for the GERLUMPH part <<<<<<<<<<<<")
 out = {}
 out["profile"] = myinput["profile"]
 
@@ -125,4 +125,5 @@ out["errbase"] = errbase
 with open(myinput["path_to_out"] + str(myinput["outdir"]) + '/input_cpp.json','w') as outfile:
     json.dump(out,outfile)
 
-print 'Done.'
+print('Done.')
+
