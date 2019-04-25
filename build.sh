6b0d8a7126c9e630360203cdc54c2ce58fe58ae5
gvernard <gvernard@astro.rug.nl>
2019-04-25 16:27:23 +0200

    Minor change to make the code easy to compile outside the GERLUMPH framework.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -f
 HERE=${GERLUMPH_PATH_CODES}LSST_simulator/v2.0
+#HERE='.'
 nvcc -std=c++11 -o $HERE/gerlumph_part -lgerlumph -ljsoncpp $HERE/gerlumph_part.cpp $HERE/auxiliary_functions.cpp
